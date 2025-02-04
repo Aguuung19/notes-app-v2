bfaf7b8066252e6ca4fe383a7278e9b28d0729eb
aguuung <aguuung19@gmail.com>
2025-02-04 10:31:12 +0800

    react parse (body note item)

--- src/components/NoteItemBody.jsx
+++ src/components/NoteItemBody.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { showFormattedDate } from "../utils/formatedDate";
+import parse from "html-react-parser";
 
 function NoteItemBody({ title, body, createdAt }) {
     return (
         <div className="note-item__content">
             <h3 className="note-item__title">{title}</h3>
             <p className="note-item__createAt">{showFormattedDate(createdAt)}</p>
-            <p className="note-item__body">{body}</p>
+            <p className="note-item__body">{parse(body)}</p>
         </div>
     );
 }
